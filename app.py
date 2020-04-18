e4e277319cf63e7a52f6c8146d5a7b3081cf859b
JOHN MUCHIRI <johnmuchirim@gmail.com>
2020-04-19 01:35:20 +0300

    newlo

--- app.py
+++ app.py
@@ -18,7 +18,7 @@ import dash
 
 
 app = dash.Dash()
-#server = app.server
+server = app.server
 
 
 
@@ -42,24 +42,7 @@ def readjsontodataframe(url):
 
 
 # Create global chart template
-mapbox_access_token = "pk.eyJ1IjoiamFja2x1byIsImEiOiJjajNlcnh3MzEwMHZtMzNueGw3NWw5ZXF5In0.fk8k06T96Ml9CLGgKmk81w"
 
-layout = dict(
-    autosize=True,
-    automargin=True,
-    margin=dict(l=30, r=30, b=20, t=40),
-    hovermode="closest",
-    plot_bgcolor="#F9F9F9",
-    paper_bgcolor="#F9F9F9",
-    legend=dict(font=dict(size=10), orientation="h"),
-    title="Satellite Overview",
-    mapbox=dict(
-        accesstoken=mapbox_access_token,
-        style="light",
-        center=dict(lon=-78.05, lat=42.54),
-        zoom=7,
-    ),
-)
 
 # Create app layout
 app.layout = html.Div(
@@ -501,4 +484,6 @@ def update_figure(well_statuses):
 
 # Add the server clause:
 if __name__ == '__main__':
-    app.run_server(debug=False)
+    app.run_server()
+
+
